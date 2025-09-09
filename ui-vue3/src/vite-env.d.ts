d4f9b7c61afd5e2ababf3e9bf0329418e10b1ab7
yuesf1 <yuesf1@gdii-yueyun.com>
2025-09-09 14:15:41 +0800

    优化后端登录

---
+++ ui-vue3/src/vite-env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_ENV_CONFIG: string
+  readonly VITE_CURRENT_ENV: string
+  readonly VITE_IS_DEV: string
+  readonly VITE_IS_DEBUG: string
+  readonly VITE_BASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
